da101175cbe86523f8ba7cad1ef67f1229ddb3ce
Johan Jatko <armedguy@xr.gs>
2016-12-14 11:35:07 +0100

    0.0.5

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'snek-orm',
   packages = ['snek', 'snek.processors','snek.models'], # this must be the same as the name above
-  version = '0.0.4',
+  version = '0.0.5',
   description = 'ORM library created just for fun',
   author = 'Johan Jatko',
   author_email = 'armedguy@ludd.ltu.se',
   url = 'https://github.com/ArmedGuy/Snek', # use the URL to the github repo
-  download_url = 'https://github.com/ArmedGuy/Snek/tarball/0.0.4',
+  download_url = 'https://github.com/ArmedGuy/Snek/tarball/0.0.5',
   keywords = ['no','step','on','snek','orm'], # arbitrary keywords
   classifiers = []
 )
